55b1a2e4d0ca88123eb13ffcf0159dca832d00cc
Александр Лаврентьев <alexlawr284@gmail.com>
2025-11-16 21:54:32 +0300

    feat(model): Создать модель данных ProcessInfo

---
+++ app/src/main/java/ru/university/student/model/ProcessInfo.java
@@ -0,0 +1,40 @@
+package ru.university.student.model;
+
+import java.util.Objects;
+
+public class ProcessInfo {
+    private final int pid;
+    private final String name;
+    private final double cpuUsage;
+    private final long memoryUsageMb;
+
+    public ProcessInfo(int pid, String name, double cpuUsage, long memoryUsageMb) {
+        this.pid = pid;
+        this.name = name;
+        this.cpuUsage = cpuUsage;
+        this.memoryUsageMb = memoryUsageMb;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCpuUsage() {
+        return cpuUsage;
+    }
+
+    public long getMemoryUsageMb() {
+        return memoryUsageMb;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("PID: %d, CPU: %.1f%%, MEM: %dMB, Name: %s",
+                pid, cpuUsage, memoryUsageMb, name);
+    }
+    
+}
